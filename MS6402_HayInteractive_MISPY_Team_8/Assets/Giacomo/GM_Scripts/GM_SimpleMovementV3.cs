8b9cedacde6f8dd97eeba82f78c1a405ddce67dc
GiacomoMazza <postagiacomo@gmail.com>
2018-11-12 18:22:01 +0000

    GM_02
    Day 2, ending

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Giacomo/GM_Scripts/GM_SimpleMovementV3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_SimpleMovementV3 : MonoBehaviour {
+
+    public float fl_MoveSpeed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey("w"))
+        {
+            this.transform.position += new Vector3(0, fl_MoveSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            this.transform.position -= new Vector3(0, fl_MoveSpeed * Time.deltaTime, 0);
+        }
+	}
+}
