bda9cdb77f3fef08087f88b95f037a77fff8c007
U1519155 <44027181+U1519155@users.noreply.github.com>
2018-11-19 14:24:21 +0000

    HH commiting Shader

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Hassan/HH_Scripts/TransformShader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformShader : MonoBehaviour {
+    
+    Renderer rRenderer;
+    float fl_bouncing;
+
+    // Use this for initialization
+    void Start () {
+        
+        rRenderer = GetComponent<Renderer>();
+        rRenderer.material.shader = Shader.Find("Outlined/Custom Constant Width");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        fl_bouncing = Mathf.PingPong(Time.time, 1.0f);
+        rRenderer.material.SetFloat("Outline width", fl_bouncing);
+	}
+}
