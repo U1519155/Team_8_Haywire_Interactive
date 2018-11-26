b3c542ef15081f619ed090db74144f2697b6900d
U1519155 <44027181+U1519155@users.noreply.github.com>
2018-11-26 00:08:33 +0000

    HH_FirstPersonCHARACTER
    needs a bit more work, the character can freely move and look around, with an added rotation clamp so the camera does not flip upside down.
    
    [Need to do]
    add crosshair
    work on jumping

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Hassan/HH_Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Vector2 mouseLook; //keeps track of how much movement the mouse has made
+    Vector2 SmoothVector; // smooths movement of mouse
+    public float mouseSensitivity = 5.0f;
+    public float smoothing = 2.0f;
+    GameObject go_PC;
+	// Use this for initialization
+	void Start () {
+        go_PC = this.transform.parent.gameObject; //sets go_PC to the parent of the camera
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseChange = Vector2.Scale(mouseChange, new Vector2(mouseSensitivity * smoothing, mouseSensitivity * smoothing));
+        SmoothVector.x = Mathf.Lerp(SmoothVector.x, mouseChange.x, 1f / smoothing); // creates smooth movement on the x
+        SmoothVector.y = Mathf.Lerp(SmoothVector.y, mouseChange.y, 1f / smoothing);// creates smooth movement on the y 
+        mouseLook += SmoothVector; //smoothing calculations added to mouselook
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -75F, 75f); // a clamp to stop the camera from going upside down
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right); // rotates the camera up and down
+        go_PC.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, go_PC.transform.up); //rotates the camera and player left and right
+
+
+    }
+}
