7d75f4c02c6073f8d55472c5a8ac3e496e62fd4c
U1519155 <44027181+U1519155@users.noreply.github.com>
2018-12-08 18:54:52 +0000

    POC_LEVEL_added_colliders

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Hassan/HH_AI/Chase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+
+    public Transform player;
+    public float fl_Distance = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(player.position, this.transform.position) < fl_Distance)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.5f);
+            }
+        }
+	}
+}
