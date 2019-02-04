032389cec9750145e2e4915dfcea1dd77bada278
GiacomoMazza <postagiacomo@gmail.com>
2019-02-04 14:18:31 +0000

    First Commit

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_Alpha/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public string st_SceneName;
+
+    void StartScene()
+    {
+        SceneManager.LoadScene(st_SceneName);
+    }
+
+    void QuitGame()
+    {
+
+    }
+}
