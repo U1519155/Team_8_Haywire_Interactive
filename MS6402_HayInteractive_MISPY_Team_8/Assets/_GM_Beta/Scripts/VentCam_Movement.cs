0a16ae1e7ddf3f74ea2552098879c3d5998dea9b
GiacomoMazza <postagiacomo@gmail.com>
2019-03-04 18:21:37 +0000

    Giacomo
    First

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/VentCam_Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentCam_Movement : MonoBehaviour
+{
+    //Variables
+    public float fl_Speed = 1.2f;
+    public float fl_Y_Down_Value = 0;
+    public float fl_Y_Up_Value = 0;
+    public float fl_X_Down_Value = 0;
+    public float fl_X_Up_Value = 0;
+    public float fl_Z_Down_Value = 0;
+    public float fl_Z_Up_Value = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeInHierarchy == true)
+        {
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                gameObject.transform.position += new Vector3(fl_X_Down_Value * Time.deltaTime * fl_Speed, fl_Y_Down_Value * Time.deltaTime * fl_Speed, fl_Z_Down_Value * Time.deltaTime * fl_Speed);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                gameObject.transform.position += new Vector3(fl_X_Up_Value * Time.deltaTime * fl_Speed, fl_Y_Up_Value * Time.deltaTime * fl_Speed, fl_Z_Up_Value * Time.deltaTime * fl_Speed);
+            }
+        }
+    }
+}
---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/Vent_Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vent_Move : MonoBehaviour
+{
+    //Variables
+    public GameObject go_VentSystem;
+    public GameObject go_CamPosition;
+    public Camera Cam_VentSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+            Cam_VentSystem.transform.position = go_CamPosition.transform.position;
+            go_VentSystem.SetActive(true);
+        }
+    }
+}
