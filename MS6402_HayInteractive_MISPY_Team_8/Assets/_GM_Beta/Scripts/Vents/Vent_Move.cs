fb5d3dd273fef33ececa1a4b4e9d58bea4c5789c
GiacomoMazza <postagiacomo@gmail.com>
2019-03-11 18:05:46 +0000

    wTF ASHLEY

--- MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/Vents/Vent_Move.cs
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/Vents/Vent_Move.cs
@@ -7,22 +7,37 @@ public class Vent_Move : MonoBehaviour
     //Variables
     public GameObject go_VentSystem;
     public GameObject go_Player;
-    public GameObject go_CamPosition;
-    public Camera Cam_VentSystem;
+    public GameObject go_StartCamPosition;
+    public GameObject go_EndCamPosition;
+    public Camera Cam_StartVentSystem;
+    public Camera Cam_EndVentSystem;
     public GameObject go_Press_E;
-    private bool bl_InRange = false;
+    private bool bl_InRangeStart = false;
+    private bool bl_InRangeEnd = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (bl_InRange == true)
+        if (bl_InRangeStart == true)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 go_Player.gameObject.SetActive(false);
-                Cam_VentSystem.transform.position = go_CamPosition.transform.position;
+                Cam_StartVentSystem.transform.position = go_StartCamPosition.transform.position;
                 go_VentSystem.SetActive(true);
-                Cam_VentSystem.gameObject.SetActive(true);
+                Cam_StartVentSystem.gameObject.SetActive(true);
+                go_Press_E.SetActive(false);
+            }
+        }
+
+        if (bl_InRangeEnd == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                go_Player.gameObject.SetActive(false);
+                Cam_EndVentSystem.transform.position = go_EndCamPosition.transform.position;
+                go_VentSystem.SetActive(true);
+                Cam_EndVentSystem.gameObject.SetActive(true);
                 go_Press_E.SetActive(false);
             }
         }
@@ -30,16 +45,23 @@ public class Vent_Move : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "StartTrigger")
         {
             go_Press_E.SetActive(true);
-            bl_InRange = true;
+            bl_InRangeStart = true;
+        }
+
+        if (other.tag == "EndTrigger")
+        {
+            go_Press_E.SetActive(true);
+            bl_InRangeEnd = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         go_Press_E.SetActive(false);
-        bl_InRange = false;
+        bl_InRangeStart = false;
+        bl_InRangeEnd = false;
     }
 }
