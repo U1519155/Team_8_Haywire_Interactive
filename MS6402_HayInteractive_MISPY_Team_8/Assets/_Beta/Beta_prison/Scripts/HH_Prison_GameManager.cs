6383a5472564191266aae8199d571ac878ba20af
U1519155 <44027181+U1519155@users.noreply.github.com>
2019-03-11 16:17:15 +0000

    pushingcucks

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_Beta/Beta_prison/Scripts/HH_Prison_GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HH_Prison_GameManager : MonoBehaviour
+{
+    [Header("--GameObjects--")]
+    public GameObject prisonDoor;
+    public GameObject WidowDoor;
+    public GameObject exitDoor;
+
+    [Header("--Bools--")]
+    public bool meetToni = false;
+    public bool hasEscaped = false;
+
+    [Header("--Ints--")]
+    public int caughtCounter;
+    public int escapeCounter;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CaughtCounterChecker();
+    }
+
+    public void CaughtCounterChecker()
+    {
+        switch (caughtCounter)
+        {
+            case 0:
+                switch (escapeCounter)
+                {
+                    case 0:
+                        if (meetToni == true)
+                        {
+                            caughtCounter++;
+                        }
+                        if (hasEscaped == true)
+                        {
+                            escapeCounter++;
+
+                        }
+
+                        break;
+
+                    case 1:
+                        if (meetToni == false)
+                        {
+                            meetToni = true;
+
+                        }
+                        break;
+                }
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+
+        }
+    }
+}
