4c68f100b83fc8da7d531da45d54f598eb02cc24
U1519155 <44027181+U1519155@users.noreply.github.com>
2019-03-12 15:13:02 +0000

    small changes
    changing prison to work witht the same scene as the level

--- MS6402_HayInteractive_MISPY_Team_8/Assets/_Beta/Beta_prison/Scripts/HH_Prison_GameManager.cs
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_Beta/Beta_prison/Scripts/HH_Prison_GameManager.cs
@@ -3,76 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HH_Prison_GameManager : MonoBehaviour
+namespace CaughtCounter
 {
-    [Header("--GameObjects--")]
-    public GameObject prisonDoor;
-    public GameObject WidowDoor;
-    public GameObject exitDoor;
-
-    [Header("--Bools--")]
-    public bool metToni = false;    
-    public static bool hasEscaped;
-
-    [Header("--Ints--")]
-    public int caughtCounter;
-    public int escapeCounter;
-
-
-    // Start is called before the first frame update
-    void Awake()
+    public class HH_Prison_GameManager : MonoBehaviour
     {
-        DontDestroyOnLoad(gameObject);
-    
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CaughtCounterChecker();
-        print("LOL i escape? = "+hasEscaped);
-    }
 
-    public void CaughtCounterChecker()
-    {
-        switch (caughtCounter)
+        [Header("--Bools--")]
+        public bool metToni = false;
+        public static bool hasEscaped;
+
+        [Header("--Ints--")]
+        public int caughtCounter;
+        public int escapeCounter;
+
+
+        // Start is called before the first frame update
+        void Awake()
         {
-            case 0:
-                switch (escapeCounter)
-                {
-                    case 0:
-                        if (metToni == true)
-                        {
-                            caughtCounter++;
-                            SceneManager.LoadScene("Beta_LevelMap");
-                        }
-
-                        if (hasEscaped == true)
-                        {
-                            escapeCounter++;
-                            SceneManager.LoadScene("Beta_LevelMap");
-                        }
-
-                        break;
-
-                    case 1:
-                        if (metToni == false)
-                        {
-                            metToni = true;
-                            caughtCounter++;
-                        }
-                        break;
-                }
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
 
 
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CaughtCounterChecker();
+            print("LOL i escape? = " + hasEscaped);
+        }
+
+        public void CaughtCounterChecker()
+        {
+            switch (caughtCounter)
+            {
+                case 0:
+                    switch (escapeCounter)
+                    {
+                        case 0:
+                            if (metToni == true)
+                            {
+                                caughtCounter++;
+                                SceneManager.LoadScene("Beta_LevelMap");
+                            }
+
+                            if (hasEscaped == true)
+                            {
+                                escapeCounter++;
+                                SceneManager.LoadScene("Beta_LevelMap");
+                            }
+
+                            break;
+
+                        case 1:
+                            if (metToni == false)
+                            {
+                                metToni = true;
+                                caughtCounter++;
+                            }
+                            break;
+                    }
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+
+
+            }
         }
     }
 }
