57cd814cb4d8076d2f1803fcecb9bf3f53ed762c
GianlucaCircasso <gianluca.circasso@gmail.com>
2019-03-18 17:55:01 +0000

    finishing touches to AI

--- MS6402_HayInteractive_MISPY_Team_8/Assets/Gianluca/GC_Scripts/GC_AI_trigger.cs
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Gianluca/GC_Scripts/GC_AI_trigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GC_AI_Alpha : MonoBehaviour
+public class GC_AI_trigger : MonoBehaviour
 {
     public enum npc_states { patrol, investigate, search, sleeping };
     public npc_states states = npc_states.patrol;
@@ -30,6 +30,9 @@ public class GC_AI_Alpha : MonoBehaviour
 
     public Vector3 pc_position;
     public float fl_grapedistance = 10;
+    public GameObject go_player;
+    public GameObject go_guard;
+    public GameObject go_wakeuptarget;
 
 
     //raycast from trigger is ofset and doesn't look at player
@@ -50,7 +53,7 @@ public class GC_AI_Alpha : MonoBehaviour
     void Update()
     {
         go_grape = GameObject.Find("Screaming Ball(Clone)");
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * fl_RaycastLenght, Color.green);
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * fl_RaycastLenght, Color.green);
         switch (states)
         {
             case npc_states.patrol:
@@ -72,7 +75,7 @@ public class GC_AI_Alpha : MonoBehaviour
                 Npc_Sleeping();
                 break;
         }
-        
+
     }
 
 
@@ -98,23 +101,46 @@ public class GC_AI_Alpha : MonoBehaviour
 
             in_destpoint = (in_destpoint + 1) % destinations.Length;        //restart arrays after reaching last position
         }
+
+
+        
+        if (gameObject.GetComponentInChildren<GC_TriggerAI>().bl_pcinrange == true)       //CHANGES for trigger
+        {
+            //Debug.Log("jackpot");
+            go_player = GetComponentInChildren<GC_TriggerAI>().go_player;
+            gameObject.transform.LookAt(go_player.transform);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * fl_RaycastLenght, Color.red);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+            {
+                
+                if (hit.collider.gameObject.GetComponent<CharacterController>())
+                {
+                    //Debug.Log("PC hit");
+                    fl_detecting += Time.deltaTime;     //if the PC stays in sight of the agent for long enough, he becomes the target
+                    npc_agent.isStopped = true;
+                    tempdest = hit.transform;
+                    npc_agent.transform.LookAt(tempdest);       //npc keeps looking at the pc
+                    go_alertsign.SetActive(true);                   //show exclamation point as visual feedback
+                    if (fl_detecting >= fl_detecttime)      //if the pc stayed too long in sight
+                    {
+
+                        //npc_agent.destination = hit.rigidbody.position;
+                        // tempdest = hit.transform;
+                        states = npc_states.investigate;
+
+                    }
+                }
+                else
+                {
+                    fl_detecting = 0;       //reset detecting timer
+                    npc_agent.isStopped = false;        //resume walking
+                    go_alertsign.SetActive(false);      //remove exclamation point
+                }
+            }
+        }
         
 
         /*
-        if (gameObject.GetComponentInChildren<GC_TriggerAI>().bl_pcinrange == true)       //CHANGES for trigger
-        {
-            Debug.Log("jackpot");
-            Debug.Log(GetComponentInChildren<GC_TriggerAI>().bl_pcinrange);
-            pc_position = GetComponentInChildren<GC_TriggerAI>().v_position;
-            if (Physics.Raycast(transform.position, pc_position, out hit, fl_RaycastLenght))
-            {
-                Debug.Log("jackpot2");
-            }
-        }
-        Debug.Log(GetComponentInChildren<GC_TriggerAI>().bl_pcinrange);
-        */
-
-
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
         {
             if (hit.collider.gameObject.GetComponent<CharacterController>())
@@ -140,9 +166,9 @@ public class GC_AI_Alpha : MonoBehaviour
                 go_alertsign.SetActive(false);      //remove exclamation point
             }
         }
-
+        */
     }
-
+    
     void Npc_investigate()
     {
         Debug.Log("investigate");
@@ -151,11 +177,11 @@ public class GC_AI_Alpha : MonoBehaviour
         go_searchsign.SetActive(false);
         go_alertsign.SetActive(true);
         fl_detecting = 0;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+        /*if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
         {       //if npc is not looking at PC - this will be changed with proper cone + raycast
             if (!hit.collider.gameObject.GetComponent<CharacterController>())
             {
-                
+
                 fl_losing += Time.deltaTime;
                 if (fl_losing >= fl_losttime)
                 {
@@ -171,10 +197,31 @@ public class GC_AI_Alpha : MonoBehaviour
         if (npc_agent.remainingDistance == 0)
         {
             states = npc_states.search;
+        }*/
+        if (gameObject.GetComponentInChildren<GC_TriggerAI>().bl_pcinrange == false)       //CHANGES for trigger
+        {
+            fl_losing += Time.deltaTime;
+            if (fl_losing >= fl_losttime)
+            {
+                Debug.Log("pc lost");
+                states = npc_states.search;
+            }
+        }
+        else
+        {
+            go_player = GetComponentInChildren<GC_TriggerAI>().go_player;
+            gameObject.transform.LookAt(go_player.transform);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+            {
+                if (hit.collider.gameObject.GetComponent<CharacterController>())
+                {
+                    fl_losing = 0;
+                }
+            }
         }
     }
 
-    void Npc_search ()
+    void Npc_search()
     {
         Debug.Log("search");
         fl_sleeping = 0;
@@ -193,12 +240,27 @@ public class GC_AI_Alpha : MonoBehaviour
             {
                 npc_agent.destination = transform.position + new Vector3(Random.Range(-fl_searchrange, fl_searchrange), 0, Random.Range(-fl_searchrange, fl_searchrange));
             }
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+            /*if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
             {
                 if (hit.collider.gameObject.GetComponent<CharacterController>())
                 {
                     tempdest = hit.transform;
                     states = npc_states.investigate;
+                }
+            }*/
+            if (gameObject.GetComponentInChildren<GC_TriggerAI>().bl_pcinrange == true)
+            {
+                go_player = GetComponentInChildren<GC_TriggerAI>().go_player;
+                gameObject.transform.LookAt(go_player.transform);
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * fl_RaycastLenght, Color.red);
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+                {
+
+                    if (hit.collider.gameObject.GetComponent<CharacterController>())
+                    {
+                        tempdest = hit.transform;
+                        states = npc_states.investigate;
+                    }
                 }
             }
         }
@@ -224,10 +286,10 @@ public class GC_AI_Alpha : MonoBehaviour
     void Npc_wakeup()
     {
         
-        GameObject go_wakeuptarget;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+        //GameObject go_wakeuptarget;
+        /*if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
         {
-            if (hit.collider.gameObject.GetComponent<GC_AI_Alpha>().states == npc_states.sleeping)
+            if (hit.collider.gameObject.GetComponent<GC_AI_trigger>().states == npc_states.sleeping)
             {
                 Debug.Log("going to wake up");
                 go_wakeuptarget = hit.collider.gameObject;
@@ -237,11 +299,27 @@ public class GC_AI_Alpha : MonoBehaviour
                     go_wakeuptarget.SendMessage("Npc_Wakeupself", SendMessageOptions.DontRequireReceiver);
                 }
             }
-        }
+        }*/
 
+        if (gameObject.GetComponentInChildren<GC_TriggerAI>().go_guard != null)
+        {
+            Debug.Log("detected sleeping");
+            go_wakeuptarget = GetComponentInChildren<GC_TriggerAI>().go_guard;
+            gameObject.transform.LookAt(go_wakeuptarget.transform);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fl_RaycastLenght))
+            {
+                Debug.Log("going to wake up");
+                npc_agent.destination = go_wakeuptarget.transform.position;
+                if (npc_agent.remainingDistance < 1)
+                {
+                    go_wakeuptarget.SendMessage("Npc_Wakeupself", SendMessageOptions.DontRequireReceiver);
+                    GetComponentInChildren<GC_TriggerAI>().go_guard = null;
+                }
+            }
+        }
     }
 
-    void Npc_Wakeupself ()
+    void Npc_Wakeupself()
     {
         Debug.Log("waking up");
         go_sleepsign.SetActive(false);
--- MS6402_HayInteractive_MISPY_Team_8/Assets/Gianluca/GC_Scripts/GC_TriggerAI.cs
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Gianluca/GC_Scripts/GC_TriggerAI.cs
@@ -8,6 +8,7 @@ public class GC_TriggerAI : MonoBehaviour
     public Vector3 v_position;
     public bool bl_pcinrange;
     public GameObject go_player;
+    public GameObject go_guard;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +25,26 @@ public class GC_TriggerAI : MonoBehaviour
             v_position = go_player.transform.position;
         }
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<GC_AI_trigger>().states == GC_AI_trigger.npc_states.sleeping)
+        {
+            go_guard = other.gameObject;
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject == go_player)
         {
             bl_pcinrange = false;
             v_position=  Vector3.zero;
+        }
+
+        if (other.gameObject.GetComponent<GC_AI_trigger>())
+        {
+            go_guard = null;
         }
     }
 }
