20627873a187d3bdbeb4eaa1425a843f266a12fd
GiacomoMazza <postagiacomo@gmail.com>
2019-04-07 03:03:52 +0100

    EndOfDay

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/Environmental_Narrative/GM_ShowUp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_ShowUp : MonoBehaviour
+{
+    public GameObject go_Monitor;
+    public GameObject go_Napkin;
+    public GameObject go_Paperwork;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        go_Monitor.SetActive(false);
+        go_Napkin.SetActive(false);
+        go_Paperwork.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Paperwork")
+        {
+            go_Paperwork.SetActive(true);
+        }
+
+        if (other.tag == "Napkin")
+        {
+            go_Napkin.SetActive(true);
+        }
+
+        if (other.tag == "Monitor")
+        {
+            go_Monitor.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Paperwork")
+        {
+            go_Paperwork.SetActive(false);
+        }
+
+        if (other.tag == "Napkin")
+        {
+            go_Napkin.SetActive(false);
+        }
+
+        if (other.tag == "Monitor")
+        {
+            go_Monitor.SetActive(false);
+        }
+    }
+}
