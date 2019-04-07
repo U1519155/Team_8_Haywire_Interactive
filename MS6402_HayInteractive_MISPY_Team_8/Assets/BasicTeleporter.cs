f4db849c07fcb02260b60ada95ec4c3cf73602f1
U1519155 <44027181+U1519155@users.noreply.github.com>
2019-04-07 23:35:11 +0100

    pushing teleporter

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/BasicTeleporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTeleporter : MonoBehaviour
+{
+    public Transform endPoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterController>())
+        {
+
+            other.transform.position = endPoint.position;
+
+        }
+    }
+}
+
+  
