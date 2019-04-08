51a788e8beccae7154fe40eba8e6b8713e67c37d
GianlucaCircasso <gianluca.circasso@gmail.com>
2019-04-08 16:06:40 +0100

    ending

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/Gianluca/GC_Scripts/GC_Diamondstuff.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GC_Diamondstuff : MonoBehaviour
+{
+
+    public GameObject go_player;
+    public GameObject go_diamond;
+    public GameObject go_endwithdiamond;
+    public GameObject go_endnodiamond;
+
+    public Text tx_take;
+    [HideInInspector] public bool bl_doit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        go_player = GameObject.FindGameObjectWithTag("Player");
+        tx_take.text = "";
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == go_player)
+        {
+            tx_take.text = "ok";
+            bl_doit=true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        tx_take.text = "";
+        bl_doit = false;
+    }
+
+    private void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                go_diamond.SetActive(false);
+                go_endnodiamond.SetActive(false);
+                go_endwithdiamond.SetActive(true);
+                tx_take.text = "boi";
+            }
+    }
+}
