d1c910fe2a23e3d2194a98ed003bcba2e2d4675f
GiacomoMazza <postagiacomo@gmail.com>
2019-05-07 20:26:37 +0100

    BlackScreen

---
+++ MS6402_HayInteractive_MISPY_Team_8/Assets/_GM_Beta/Scripts/Environmental_Narrative/GM_LoadAfterSeconds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM_LoadAfterSeconds : MonoBehaviour
+{
+    //Variables
+    public string st_NameScene;
+    public float fl_SecondsToElapse = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadMeSeconds", fl_SecondsToElapse);
+    }
+
+    void LoadMeSeconds()
+    {
+        SceneManager.LoadScene(st_NameScene);
+    }
+}
